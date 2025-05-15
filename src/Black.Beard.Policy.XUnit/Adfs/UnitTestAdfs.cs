1a6559aa6744f28f6880985df56d38e75d75d314
gael beard <gaelgael5@gmail.com>
2025-05-15 07:43:54 +0200

    replace adfs

--- src/Black.Beard.Policy.XUnit/Adfs/UnitTestAdfs.cs
+++ src/Black.Beard.Policy.XUnit/Adfs/UnitTestAdfs.cs
@@ -1,53 +1,49 @@
 
 
-//// Ignore Spelling: Adfs Api
+// Ignore Spelling: Adfs Api
 
-//using Bb.Adfs;
-//using Microsoft.Extensions.Logging;
-//using System.Runtime.Versioning;
+using Bb.Adfs;
+using Microsoft.Extensions.Logging;
+using System.Runtime.Versioning;
 
-//namespace Black.Beard.Policy.XUnit.Adfs
-//{
+namespace Black.Beard.Policy.XUnit.Adfs
+{
 
-//    [SupportedOSPlatform("windows")]
-//    public class UnitTestAdfs
-//    {
+    [SupportedOSPlatform("windows")]
+    public class UnitTestAdfs
+    {
 
-//        [Fact]
-//        public void TestCreateApiOnAdfs()
-//        {
+        [Fact]
+        public void TestCreateApiOnAdfs()
+        {
 
-//            string salt = "my_salt";
-//            string firstname = "firstname";
-//            string lastname = "lastname";
-//            string username = "username";
-//            string email = "gaelgael5@gmail.com";
-//            string organisation = "black.beard";
+            string salt = "my_salt";
+            string firstname = "firstname";
+            string lastname = "lastname";
+            string username = "username";
+            string email = "gaelgael5@gmail.com";
+            string organisation = "black.beard";
 
-
-//            string adfsServer = "adfs.company.com";
-//            string adfsDomain = "DC=company,DC=com";
-//            string adfsUser = "administrator";
-//            string adfsPassword = "password123";
-
-
-//            ILogger logger = LoggerFactory.Create(builder => builder.AddConsole()).CreateLogger<AdfsConnection>();
-
-//            using (var adfs = new AdfsConnection(logger))
-//            {
-
-//                if (adfs.Connect(adfsServer, adfsDomain, adfsUser, adfsPassword))
-//                {
-//                    adfs.CreateApiKey(salt, username, firstname, lastname, email, organisation, "group1");
-//                }
-//                else
-//                    Assert.Fail("Connection failed");
-
-//            }
-
-//        }
-
-//    }
+            string adfsServer = "adfs.company.com";
+            string adfsDomain = "DC=company,DC=com";
+            string adfsUser = "administrator";
+            string adfsPassword = "password123";
 
 
-//}
+            ILogger logger = LoggerFactory.Create(builder => builder.AddConsole()).CreateLogger<AdfsConnection>();
+            using (var adfs = new AdfsConnection(logger))
+            {
+                if (adfs.Connect(adfsServer, adfsDomain, adfsUser, adfsPassword))
+                {
+                    adfs.CreateApiKey(salt, username, firstname, lastname, email, organisation, "group1");
+                }
+                else
+                    Assert.Fail("Connection failed");
+            }
+
+        }
+
+    }
+
+
+}
